f927abe96d2da4fc1f297c1e08ca90a87fecad49
MrBusiness87 <bbartula87@gmail.com>
2020-06-12 21:11:52 -0400

    06/12/2020-Pre-final Push of game

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 const start = document.getElementById("start");
+const h1 = document.getElementById("h1");
+const h2 = document.getElementById("h2");
 const quiz = document.getElementById("quiz");
 const question = document.getElementById("question");
 const qImg = document.getElementById("qImg");
@@ -10,121 +12,146 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 const scoreDiv = document.getElementById("scoreContainer");
 
-let questions = [
-    {
-        question : "What does HTML stand for?",
-        imgSrc : "./Assets/html.png",
-        choiceA : "Correct",
-        choiceB : "Wrong",
-        choiceC : "Wrong",
-        correct : "A"
-    },{
-        question : "What does CSS stand for?",
-        imgSrc : "./Assets/css.png",
-        choiceA : "Wrong",
-        choiceB : "Correct",
-        choiceC : "Wrong",
-        correct : "B"
-    },{
-        question : "What does JS stand for?",
-        imgSrc : "./Assets/js.png",
-        choiceA : "Wrong",
-        choiceB : "Wrong",
-        choiceC : "Correct",
-        correct : "C"
-    }
+let questions = [{
+    question: "What is the purpose of HTML?",
+    imgSrc: "./Assets/html.png",
+    choiceA: "Format and Display Content in Web Browser.",
+    choiceB: "Create Actions on Web Page.",
+    choiceC: "Changes Text on Web Page only.",
+    correct: "A",
+  },
+  {
+    question: "What are these:<> called?",
+    imgSrc: "./Assets/css.png",
+    choiceA: "Targets",
+    choiceB: "Tags",
+    choiceC: "Corners",
+    correct: "B",
+  },
+  {
+    question: "What are the three main file types for coding a page?",
+    imgSrc: "./Assets/js.png",
+    choiceA: "HIJK/CDF/JK",
+    choiceB: "HTTP/C++/JK",
+    choiceC: "HTML/CSS/JS",
+    correct: "C",
+  },
+  {
+    question: "What is the purpose of CSS?",
+    imgSrc: "./Assets/js.png",
+    choiceA: "To provide code for creating another page.",
+    choiceB: "To provide information for styling a page.",
+    choiceC: "To create pictures.",
+    correct: "B",
+  },
+  {
+    question: "What are the three main file types for coding a page?",
+    imgSrc: "./Assets/js.png",
+    choiceA: "HIJK/CDF/JK",
+    choiceB: "HTTP/C++/JK",
+    choiceC: "HTML/CSS/JS",
+    correct: "C",
+  },
 ];
 
 const lastQuestion = questions.length - 1;
 let runningQuestion = 0;
-let count = 0;
-const questionTime = 10;
-const gaugeWidth = 150;
-const gaugeUnit = gaugeWidth / questionTime;
+let count = 200;
+const quizTime = 300;
+const gaugeWidth = 225;
+const gaugeUnit = gaugeWidth / quizTime;
 let TIMER;
 let score = 0;
 
-function renderQuestion(){
-    let q = questions[runningQuestion];
-    
-    question.innerHTML = "<p>"+ q.question +"</p>";
-    qImg.innerHTML = "<img src="+ q.imgSrc +">";
-    choiceA.innerHTML = q.choiceA;
-    choiceB.innerHTML = q.choiceB;
-    choiceC.innerHTML = q.choiceC;
+function renderQuestion() {
+  let q = questions[runningQuestion];
+
+  question.innerHTML = "<p>" + q.question + "</p>";
+  qImg.innerHTML = "<img src=" + q.imgSrc + ">";
+  choiceA.innerHTML = q.choiceA;
+  choiceB.innerHTML = q.choiceB;
+  choiceC.innerHTML = q.choiceC;
 }
 
-start.addEventListener("click",startQuiz);
+start.addEventListener("click", startQuiz);
 
-function startQuiz(){
-    start.style.display = "none";
+function startQuiz() {
+  h1.style.display = "none";
+  h2.style.display = "none";
+  start.style.display = "none";
+  renderQuestion();
+  quiz.style.display = "block";
+  renderProgress();
+  renderCounter();
+  TIMER = setInterval(renderCounter, 1000);
+}
+
+function renderProgress() {
+  for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
+    progress.innerHTML += "<div class='prog' id=" + qIndex + "></div>";
+  }
+}
+
+function renderCounter() {
+  if (count >= 0) {
+    counter.innerHTML = count;
+    timeGauge.style.width = count * gaugeUnit + "px";
+    count--;
+  } else {
+    answerIsWrong();
+    if (runningQuestion < lastQuestion) {
+      runningQuestion++;
+      renderQuestion();
+    } else {
+      clearInterval(TIMER);
+      scoreRender();
+    }
+  }
+}
+
+function checkAnswer(answer) {
+  if (answer == questions[runningQuestion].correct) {
+    score++;
+    answerIsCorrect();
+  } else {
+    answerIsWrong();
+  }
+  if (runningQuestion < lastQuestion) {
+    runningQuestion++;
     renderQuestion();
-    quiz.style.display = "block";
-    renderProgress();
-    renderCounter();
-    TIMER = setInterval(renderCounter,1000);
+  } else {
+    clearInterval(TIMER);
+    scoreRender();
+  }
 }
 
-function renderProgress(){
-    for(let qIndex = 0; qIndex <= lastQuestion; qIndex++){
-        progress.innerHTML += "<div class='prog' id="+ qIndex +"></div>";
-    }
+function answerIsCorrect() {
+  document.getElementById(runningQuestion).style.backgroundColor = "green";
 }
 
-function renderCounter(){
-    if(count <= questionTime){
-        counter.innerHTML = count;
-        timeGauge.style.width = count * gaugeUnit + "px";
-        count++
-    }else{
-        count = 0;
-        answerIsWrong();
-        if(runningQuestion < lastQuestion){
-            runningQuestion++;
-            renderQuestion();
-        }else{
-            clearInterval(TIMER);
-            scoreRender();
-        }
-    }
+function answerIsWrong() {
+  document.getElementById(runningQuestion).style.backgroundColor = "red";
 }
 
-function checkAnswer(answer){
-    if( answer == questions[runningQuestion].correct){
-        score++;
-        answerIsCorrect();
-    }else{
-        answerIsWrong();
-    }
-    count = 0;
-    if(runningQuestion < lastQuestion){
-        runningQuestion++;
-        renderQuestion();
-    }else{
-        clearInterval(TIMER);
-        scoreRender();
-    }
+function scoreRender() {
+  scoreDiv.style.display = "block";
+
+  const scorePercent = Math.round((100 * score) / questions.length);
+
+  let img =
+    scorePercent >= 80 ?
+    "./Assets/5.png" :
+    scorePercent >= 60 ?
+    "./Assets/4.png" :
+    scorePercent >= 40 ?
+    "./Assets/3.png" :
+    scorePercent >= 20 ?
+    "./Assets/2.png" :
+    "./Assets/1.png";
+
+  scoreDiv.innerHTML = "<img src=" + img + ">";
+  scoreDiv.innerHTML += "<p>" + scorePercent + "%</p>";
+  // createElement.block = "save score";
 }
 
-function answerIsCorrect(){
-    document.getElementById(runningQuestion).style.backgroundColor = "green";
-}
-
-function answerIsWrong(){
-    document.getElementById(runningQuestion).style.backgroundColor = "red";
-}
-
-function scoreRender(){
-    scoreDiv.style.display = "block";
-    
-    const scorePercent = Math.round(100 * score/questions.length);
-
-    let img = (scorePercent >= 80) ? "./Assets/5.png" :
-              (scorePercent >= 60) ? "./Assets/4.png" :
-              (scorePercent >= 40) ? "./Assets/3.png" :
-              (scorePercent >= 20) ? "./Assets/2.png" :
-              "./Assets/1.png";
-    
-    scoreDiv.innerHTML = "<img src="+ img +">";
-    scoreDiv.innerHTML += "<p>"+ scorePercent +"%</p>";
-}
+// const username = 
